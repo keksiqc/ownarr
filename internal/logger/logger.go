68a59bc1b218c412d4afc79d06808c5cfe9b049f
marlonhotz07 <marlonhotz07@gmail.com>
2025-09-24 11:24:38 +0000

    feat: add charmbracelet/log dependency for terminal logging

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,37 +2,36 @@ package logger
 
 import (
 	"context"
-	"log/slog"
 	"os"
+	"github.com/charmbracelet/log"
 )
 
 type Logger struct {
-	*slog.Logger
+	*log.Logger
 }
 
 func New(level string) *Logger {
-	var logLevel slog.Level
-	switch level {
-	case "debug":
-		logLevel = slog.LevelDebug
-	case "info":
-		logLevel = slog.LevelInfo
-	case "warn", "warning":
-		logLevel = slog.LevelWarn
-	case "error":
-		logLevel = slog.LevelError
-	default:
-		logLevel = slog.LevelInfo
-	}
-
-	opts := &slog.HandlerOptions{
-		Level: logLevel,
-	}
-
-	handler := slog.NewTextHandler(os.Stdout, opts)
-	logger := slog.New(handler)
+	logger := log.NewWithOptions(os.Stdout, log.Options{
+		ReportTimestamp: true,
+		Level:           getLogLevel(level),
+	})
 
 	return &Logger{logger}
+}
+
+func getLogLevel(level string) log.Level {
+	switch level {
+	case "debug":
+		return log.DebugLevel
+	case "info":
+		return log.InfoLevel
+	case "warn", "warning":
+		return log.WarnLevel
+	case "error":
+		return log.ErrorLevel
+	default:
+		return log.InfoLevel
+	}
 }
 
 func (l *Logger) With(args ...any) *Logger {
